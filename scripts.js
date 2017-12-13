31283320a38a30497c2d6899ce0eecf252c22ab6
Jonathan Pool <pool@stulta.com>
2017-12-13 07:45:22 -0800

    Continued development. Adopted bootstrap grid principles for layout. Animated buttons.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,24 @@
 'use strict';
 
-$(window).on('load', function () {
-  $('.carousel-image').each(function() {
-    if (this.naturalWidth > this.naturalHeight) {
-      $(this).removeClass('w-100').addClass('h-100');
-    }
-    else {
-      $(this).removeClass('h-100').addClass('w-100');
-    }
+// $(window).on('load', function () {
+//   $('.carousel-image').each(function() {
+//     if (this.naturalWidth > this.naturalHeight) {
+//       $(this).removeClass('w-100').addClass('h-100');
+//     }
+//     else {
+//       $(this).removeClass('h-100').addClass('w-100');
+//     }
+//   });
+// });
+
+$(document).ready(function() {
+  $('#question .btn').filter(':first').click(function() {
+    $('#question').addClass('d-none');
+    $('#answer').removeClass('d-none');
+    var answerSec = $('#answer');
+  });
+  $('#answer .reminder').filter(':first').click(function() {
+    $('#answer').addClass('d-none');
+    $('#question').removeClass('d-none');
   });
 });
