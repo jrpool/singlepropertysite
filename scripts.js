1ef5f381cc4178e8b383e301d072b7f8a1cb6f4c
Jonathan Pool <pool@stulta.com>
2017-12-20 10:17:49 -0800

    Finished populating and linking sections. Revised mail link. Left graphic inclusion for later.

--- scripts.js
+++ scripts.js
@@ -11,19 +11,19 @@
 //   });
 // });
 
-var hResize = function() {
-  ['1', '2', '4'].forEach(function(tagNum) {
-    var targetElements = document.querySelectorAll('.h' + tagNum);
+var fontResize = function() {
+  ['h1', 'h2', 'h4', 'list'].forEach(function(className) {
+    var targetElements = document.querySelectorAll('.' + className);
     if (window.innerWidth > 500) {
       targetElements.forEach(function(element) {
-        element.classList.remove('h' + tagNum + '-minimum');
-        element.classList.add('h' + tagNum + '-proportional');
+        element.classList.remove(className + '-minimum');
+        element.classList.add(className + '-proportional');
       });
     }
     else {
       targetElements.forEach(function(element) {
-        element.classList.remove('h' + tagNum + '-proportional');
-        element.classList.add('h' + tagNum + '-minimum');
+        element.classList.remove(className + '-proportional');
+        element.classList.add(className + '-minimum');
       });
     }
   });
@@ -41,7 +41,7 @@ var creditResize = function() {
   }
 };
 
-var replaceSection = function(trigger, replacerSelector, replaceeSelector) {
+var replaceSection = function(trigger, replaceeSelector, replacerSelector) {
   document.querySelector(trigger).onclick = function() {
     var replacer = document.querySelector(replacerSelector);
     var replacee = document.querySelector(replaceeSelector);
@@ -52,40 +52,56 @@ var replaceSection = function(trigger, replacerSelector, replaceeSelector) {
     replacee.style.opacity = 0;
     window.setTimeout(function() {
       replacee.style.display = 'none';
-    }, 2000);
+    }, 1500);
   };
 };
 
 window.onload = function() {
-  var overtSectionIDs = ['#question'];
-  var latentSectionIDs = ['#answer', '#details', '#join'];
-  var latentSectionClasses = ['.detail-list'];
-  latentSectionClasses.forEach(function(sectionClass) {
-    var sections = document.querySelectorAll(sectionClass);
-    sections.forEach(function(section) {
-      section.style.display = 'none';
-      section.style.opacity = 0;
-    });
+  document.querySelectorAll('.later').forEach(function(section) {
+    section.style.display = 'none';
+    section.style.opacity = 0;
   });
-  latentSectionIDs.forEach(function(sectionID) {
-    document.querySelector(sectionID).style.display = 'none';
-    document.querySelector(sectionID).style.opacity = 0;
+  document.querySelectorAll('.initial').forEach(function(section) {
+    section.style.display = 'block';
+    section.style.opacity = 1;
   });
-  overtSectionIDs.forEach(function(sectionID) {
-    document.querySelector(sectionID).style.display = 'block';
-    document.querySelector(sectionID).style.opacity = 1;
-  });
-  hResize();
+  fontResize();
   creditResize();
-  replaceSection('#question .btn-lg', '#answer', '#question');
-  replaceSection('#answer .back', '#question', '#answer');
-  replaceSection('#answer .btn-lg', '#details', '#answer');
-  replaceSection('#details .back', '#answer', '#details');
-  replaceSection('#details .btn-lg', '#join', '#details');
-  replaceSection('#join .back', '#details', '#join');
+  var replacements = [
+    ['#question .btn-lg', '#question', '#answer'],
+    ['#answer .back', '#answer', '#question'],
+    ['#answer .btn-lg', '#answer', '#details'],
+    ['#details .back', '#details', '#answer'],
+    ['#details .btn-lg', '#details', '#join'],
+    ['#members-more', '#details', '#members-list'],
+    ['#members-list .btn-lg', '#members-list', '#life-list'],
+    ['#members-list .back', '#members-list', '#details'],
+    ['#life-more', '#details', '#life-list'],
+    ['#life-list .btn-lg', '#life-list', '#coop-list'],
+    ['#life-list .back', '#life-list', '#details'],
+    ['#coop-more', '#details', '#coop-list'],
+    ['#coop-list .btn-lg', '#coop-list', '#gov-list'],
+    ['#coop-list .back', '#coop-list', '#details'],
+    ['#gov-more', '#details', '#gov-list'],
+    ['#gov-list .btn-lg', '#gov-list', '#common-list'],
+    ['#gov-list .back', '#gov-list', '#details'],
+    ['#common-more', '#details', '#common-list'],
+    ['#common-list .btn-lg', '#common-list', '#apt-list'],
+    ['#common-list .back', '#common-list', '#details'],
+    ['#apt-more', '#details', '#apt-list'],
+    ['#apt-list .btn-lg', '#apt-list', '#walk-list'],
+    ['#apt-list .back', '#apt-list', '#details'],
+    ['#walk-more', '#details', '#walk-list'],
+    ['#walk-list .btn-lg', '#walk-list', '#join'],
+    ['#walk-list .back', '#walk-list', '#details'],
+    ['#join .back', '#join', '#details']
+  ];
+  replacements.forEach(function(agenda) {
+    replaceSection(agenda[0], agenda[1], agenda[2]);
+  });
 };
 
 window.onresize = function() {
-  hResize();
+  fontResize();
   creditResize();
 };
