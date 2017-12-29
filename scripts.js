da6f997e1436beec91d287d87432c26101a4dc1a
Jonathan Pool <pool@stulta.com>
2017-12-29 11:20:05 -0800

    Implemented picture tour.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 'use strict';
 
+// Define a handler to make fonts’ sizes responsive to window width.
 var fontResize = function() {
   [['.h1', 'h1'], ['.h2', 'h2'], ['.h3', 'h3'], ['.h4', 'h4'], ['.h5', 'h5']]
   .forEach(function(spec) {
-    var targetElements = document.querySelectorAll(spec[0]);
+    var targetElements = Array.from(document.querySelectorAll(spec[0]));
     if (window.innerWidth > 500) {
       targetElements.forEach(function(element) {
         element.classList.remove(spec[1] + '-minimum');
@@ -19,6 +20,7 @@ var fontResize = function() {
   });
 };
 
+// Define a handler to make the credit’s size responsive to window width.
 var creditResize = function() {
   var targetElement = document.querySelector('.credit');
   if (window.innerWidth > 625) {
@@ -31,6 +33,7 @@ var creditResize = function() {
   }
 };
 
+// Define a function to set the focus of a section.
 var setFocusOf = function(section) {
   var priorFocus = section.querySelector('[had-focus]');
   if (priorFocus) {
@@ -68,6 +71,7 @@ var setFocusOf = function(section) {
   }
 };
 
+// Define listeners and handler for a button navigating between sections.
 var replaceSection = function(replacee, replacer) {
   // Manage focus in old section.
   var activeElement = document.activeElement;
@@ -94,7 +98,7 @@ var replaceSection = function(replacee, replacer) {
   }
   replacer.scrollTop = 0;
 };
-
+// Define listeners.
 var replaceOnClick = function(trigger, replacee, replacer) {
   trigger.onclick = function() {
     replaceSection(replacee, replacer);
@@ -106,6 +110,79 @@ var replaceOnClick = function(trigger, replacee, replacer) {
   };
 };
 
+// Define listeners and handler for verbosity toggling.
+// Handler.
+var toggleVerbosity = function(button) {
+  var sublist = button.parentNode.nextElementSibling;
+  if (sublist.style.display === 'none') {
+    button.querySelector('.do').style.display = 'none';
+    button.querySelector('.undo').style.display = 'inline';
+    sublist.style.display = 'block';
+    sublist.style.opacity = 1;
+  }
+  else {
+    button.querySelector('.do').style.display = 'inline';
+    button.querySelector('.undo').style.display = 'none';
+    sublist.style.display = 'none';
+    sublist.style.opacity = 0;
+  }
+};
+// Listeners.
+var toggleVerbosityOnClick = function(button) {
+  button.onclick = toggleVerbosity;
+  button.onkeydown = function(event) {
+    if (event.key === ' ' || event.key === 'Enter') {
+      toggleVerbosity(button);
+    }
+  };
+};
+
+// Define listeners and handlers for a tour stop.
+// Handler utility.
+var showPicOf = function(tourSection, newStop) {
+  tourSection.setAttribute('stop', newStop);
+  tourSection.style.background-image
+    = 'url(resources/images/' + tourData[newStop][0] + ')';
+  tourSection.querySelector('.carousel-caption').textContent
+    = tourData[newStop][1];
+};
+// Handler for next.
+var showNextPic = function(tourData) {
+  var tourSection = document.querySelector('#tour');
+  var tourStop = Number.parseInt(tourSection.getAttribute('stop'));
+  var nextStop = tourStop === tourData.length - 1 ? 0 : tourStop + 1;
+  showPicOf(tourSection, nextStop);
+};
+// Handler for prior.
+var showPriorPic = function(tourData) {
+  var tourSection = document.querySelector('#tour');
+  var tourStop = Number.parseInt(tourSection.getAttribute('stop'));
+  var priorStop = tourStop === 0 ? tourData.length - 1 : tourStop - 1;
+  showPicOf(tourSection, priorStop);
+};
+// Listeners.
+var showNextPicOnClick = function(button, tourData) {
+  button.onclick = function() {
+    showNextPic(tourData);
+  };
+  button.onkeydown = function(event) {
+    if (event.key === ' ' || event.key === 'Enter') {
+      showNextPic(tourData);
+    }
+  }
+}
+var showPriorPicOnClick = function(button, tourData) {
+  button.onclick = function() {
+    showPriorPic(tourData);
+  };
+  button.onkeydown = function(event) {
+    if (event.key === ' ' || event.key === 'Enter') {
+      showPriorPic(tourData);
+    }
+  }
+}
+
+// Define a function to identify an element’s section.
 var sectionOf = function(element) {
   var parent = element.parentElement;
   if (parent.tagName === 'SECTION') {
@@ -116,66 +193,85 @@ var sectionOf = function(element) {
   }
 };
 
+// Define listeners and handler for a picture-showing button.
+// Handler.
+var showPic = function(button) {
+  var imageSection = document.querySelector('#images');
+  imageSection.style['background-image']
+    = 'url(resources/images/' + button.getAttribute('pic') + ')';
+  var buttonSection = sectionOf(button);
+  // Make back button on picture return to sending section.
+  replaceOnClick(
+    imageSection.firstElementChild, imageSection, buttonSection
+  );
+  replaceSection(buttonSection, imageSection);
+};
+// Listeners.
+var showPicOnClick = function(button) {
+  button.onclick = showPic;
+  button.onkeydown = function(event) {
+    if (event.key === ' ' || event.key === 'Enter') {
+      showPic(button);
+    }
+  };
+};
+
+var nextPic = function(currentTourStop) {
+  if (currentTourStop === )
+};
+
+// When the page is retrieved or refreshed:
 window.onload = function() {
-  document.querySelectorAll('.later').forEach(function(section) {
+  // Make all initially hidden sections invisible.
+  Array.from(document.querySelectorAll('.later')).forEach(function(section) {
     section.style.display = 'none';
     section.style.opacity = 0;
   });
-  document.querySelectorAll('.btn-info').forEach(function(button) {
+  // Make all buttons focusable.
+  Array.from(document.querySelectorAll('.btn-info')).forEach(function(button) {
     button.setAttribute('tabindex', '0');
   });
   setFocusOf(document.querySelector('#question'));
+  // Create listeners for window resizings.
   fontResize();
   creditResize();
-  document.querySelectorAll('[dest]').forEach(function(button) {
+  // Create listeners for section-replacing buttons.
+  Array.from(document.querySelectorAll('[dest]')).forEach(function(button) {
     replaceOnClick(
       button,
       sectionOf(button),
       document.querySelector('#' + button.getAttribute('dest'))
     );
   });
-  document.querySelectorAll('[pic]').forEach(function(button) {
-    var handler = function() {
-      var imageSection = document.querySelector('#images');
-      imageSection.style['background-image']
-        = 'url(resources/images/' + button.getAttribute('pic') + ')';
-      var buttonSection = sectionOf(button);
-      replaceOnClick(
-        imageSection.firstElementChild, imageSection, buttonSection
-      );
-      replaceSection(buttonSection, imageSection);
-    };
-    button.onclick = handler;
-    button.onkeydown = function(event) {
-      if (event.key === ' ' || event.key === 'Enter') {
-        handler();
-      }
-    };
+  // Create listeners for picture-showing buttons.
+  var picSpans = Array.from(document.querySelectorAll('[pic]'));
+  picSpans.forEach(showPicOnClick);
+  // Create data on tour stops.
+  var tourData = picSpans.map(function(picSpan) {
+    return [
+      picSpan.getAttributeOf('pic'), sectionOf(picSpan).getAttributeOf('tour-intro')
+    ].filter(function(element) {
+      return element[1];
+    });
   });
-  document.querySelectorAll('span.toggle-child')
+  // Initialize tour section.
+  document
+  .querySelector('#tour > [dest-pic]')
+  .setAttribute('dest-pic', tourData[0][0]);
+  document
+  .querySelector('#tour > .carousel-caption')
+  .textContent = tourData[0][1];
+  // Create listeners for tour navigation buttons.
+  Array.from(document.querySelectorAll('.next-pic'))
   .forEach(function(button) {
-    var handler = function() {
-      var sublist = button.parentNode.nextElementSibling;
-      if (sublist.style.display === 'none') {
-        button.querySelector('.do').style.display = 'none';
-        button.querySelector('.undo').style.display = 'inline';
-        sublist.style.display = 'block';
-        sublist.style.opacity = 1;
-      }
-      else {
-        button.querySelector('.do').style.display = 'inline';
-        button.querySelector('.undo').style.display = 'none';
-        sublist.style.display = 'none';
-        sublist.style.opacity = 0;
-      }
-    };
-    button.onclick = handler;
-    button.onkeydown = function(event) {
-      if (event.key === ' ' || event.key === 'Enter') {
-        handler();
-      }
-    };
+    showNextPicOnClick(button, tourData);
   });
+  Array.from(document.querySelectorAll('.prior-pic'))
+  .forEach(function(button) {
+    showPriorPicOnClick(button, tourData);
+  // Create listeners for buttons that toggle verbosity.
+  Array.from(document.querySelectorAll('span.toggle-child'))
+  .forEach(toggleVerbosityOnClick);
 };
 
 window.onresize = function() {
